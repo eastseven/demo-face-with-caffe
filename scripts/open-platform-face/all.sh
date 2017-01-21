60e20ac207fe8e37c4086138b67a2fa290a81d54
eastseven <eastseven@foxmail.com>
2017-01-21 10:39:25 +0800

    修改all.sh，将生成的log文件存放在单独的目录下，方便清理

--- scripts/open-platform-face/all.sh
+++ scripts/open-platform-face/all.sh
@@ -7,8 +7,15 @@ if [ ! -f "$imgFile" ]; then
   wget -P `pwd`/images $imgUrl;
 fi
 
-docker run --rm -v $(pwd):/workspace caffe:cpu bash -c "/opt/caffe/build/examples/cpp_classification/classification.bin /workspace/age_deploy.prototxt /workspace/age_net.caffemodel /workspace/mean.binaryproto /workspace/age_labels.txt /workspace/images/"$imgName > ${imgName}-age.log
+imgLog=`pwd`/images.log
+if [ ! -f "$imgLog" ]; then
+  mkdir -p $imgLog;
+fi
 
-docker run --rm -v $(pwd):/workspace caffe:cpu bash -c "/opt/caffe/build/examples/cpp_classification/classification.bin /workspace/gender_deploy.prototxt /workspace/gender_net.caffemodel /workspace/mean.binaryproto /workspace/gender_labels.txt /workspace/images/"$imgName > ${imgName}-gender.log
+docker run --rm \
+ -v $(pwd):/workspace caffe:cpu bash -c "/opt/caffe/build/examples/cpp_classification/classification.bin /workspace/age_deploy.prototxt /workspace/age_net.caffemodel /workspace/mean.binaryproto /workspace/age_labels.txt /workspace/images/"$imgName > $imgLog/${imgName}-age.log
+
+docker run --rm \
+ -v $(pwd):/workspace caffe:cpu bash -c "/opt/caffe/build/examples/cpp_classification/classification.bin /workspace/gender_deploy.prototxt /workspace/gender_net.caffemodel /workspace/mean.binaryproto /workspace/gender_labels.txt /workspace/images/"$imgName > $imgLog/${imgName}-gender.log
 
 echo 'done'
