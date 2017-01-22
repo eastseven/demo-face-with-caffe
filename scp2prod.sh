35c9d7e1c62e286c0900f92195a9847985e254c7
eastseven <eastseven@foxmail.com>
2017-01-22 15:54:14 +0800

    update

--- scp2prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mvn clean package -U -Dmaven.test.skip
-#scp -r scripts/open-platform-face root@face.51ishare.com:/data/workspace/
-#scp -r scripts/open-platform-face/start.sh root@face.51ishare.com:/data/workspace/open-platform-face/
-scp -r target/*.jar root@face.51ishare.com:/data/workspace/open-platform-face/app.jar
